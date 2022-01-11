e1c91fa651e1e316b23f8c106ec1d098fcf65925
davidflores8 <davidflores126@unitec.edu>
2022-01-11 00:30:20 -0600

    Agrega a firestore

--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,12 +1,45 @@
 import React, { useState, useEffect } from 'react'
+import {db} from "../firebase"
+import { doc, setDoc } from "firebase/firestore";
 
 function Register() {
 
-    let [name, setName] = useState("")
-    let [email, setEmail] = useState("")
-    let [date, setDate] = useState("")
-    let [password, setPassword] = useState("")
-    let [password2, setPassword2] = useState("")
+    let initialState = {
+        name : "",
+        email :"",
+        date :"",
+        password2 :"",
+        password :""
+    }
+
+    let [info, setInfo] = useState({...initialState})
+
+
+
+    const handleName = event =>{
+        setInfo(prevState => ({...prevState, name: event.target.value}))
+    }
+
+    const handleEmail = event =>{
+        setInfo(prevState => ({...prevState, email: event.target.value}))
+    }
+
+    const handleDate = event =>{
+        setInfo(prevState => ({...prevState, date: event.target.value}))
+    }
+
+    const handlePassword = event =>{
+        setInfo(prevState => ({...prevState, password: event.target.value}))
+    }
+    const handlePassword2 = event =>{
+        setInfo(prevState => ({...prevState, password2: event.target.value}))
+    }
+
+    const handleSubmit  = async () =>{
+        await setDoc(doc(db, "Usuarios", info.email), {
+            info
+          });
+    }
 
     //Mensaje de comprobacion de contraseñas
 
@@ -16,33 +49,36 @@ function Register() {
             <form className = "mt-4"> 
                 <div className ="mb-3">
                     <label className = "form-label d-block w-100 mx-auto">Ingrese su nombre: </label>
-                    <input type = "text" onChange = {setName = e => {name = e.target.value } } className="form-control"/>
+                    <input type = "text" onChange = {handleName}className="form-control"/>
                 </div>
                 <div className="mb-3">
                     <label className = "form-label">Ingrese su correo electrónico: </label>
-                    <input type = "email" onChange = {setEmail = e => {email = e.target.value } } className="form-control"/>
+                    <input type = "email"  onChange = {handleEmail} className="form-control"/>
                 </div>
                 <div className="mb-3">
                     <label className = "form-label">Ingrese su fecha de nacimiento: </label>
-                    <input type = "date" onChange = {setDate = e => {date = e.target.value } } className="form-control"/>
+                    <input type = "date" onChange = {handleDate}  className="form-control"/>
                 </div>
                 <div className="mb-3">
                     <label className = "form-label">Ingrese su contraseña: </label>
-                    <input type = "password" onChange = {setPassword = e => {password = e.target.value } }className="form-control"/>
+                    <input type = "password" onChange = {handlePassword} className="form-control"/>
                 </div>
                 <div className="mb-3">
                     <label className = "form-label">Ingrese su contraseña nuevamente: </label>
-                    <input type = "password" onChange = {setPassword2 = e => {password2 = e.target.value; passwordsConfiguration();} } className ="form-control"/>
-                    <label className = "form-text"> -- {password2}</label>
+                    <input type = "password"  onChange = {handlePassword2} className ="form-control"/>
                 </div>
                 <div className = "container mb-4 mt-5">
-                   <button type="button" className="btn btn-primary d-block mx-auto">
+                   <button type="button" onClick = {handleSubmit} className="btn btn-primary d-block mx-auto">
                        Registrarse
                     </button>
                 </div>
             </form>
         </div>
     )
+
+    
 }
+
+
 
 export default Register
